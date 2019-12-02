3163dca183af7596cc05ec55c412bc0a68dc5890
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-12-02 13:59:00 -0500

    complete

--- main.js
+++ main.js
@@ -1,19 +1,51 @@
-const makeDino = function() {
+const makeDino = function(species, period, carnivore, extinct=false) {
+  return {
+    species: species,
+    period: period,
+    carnivore: carnivore,
+    extinct: extinct,
 }
-
-const makeSingular = function() {
 }
-
-const truncateSpecies = function() {
+const makeSingular = function(dino) {
+  if (!dino['species'].endsWith('us')) {
+    return dino
+  }
+  else {
+    return {
+    species: dino['species'].slice(0,-2),
+    period: dino['period'],
+    carnivore: dino['carnivore'],
+    extinct: dino['extinct'],
 }
-
-const makeExtinct = function() {
-
 }
-
+}
+const truncateSpecies = function(dino) {
+  if (dino['species'].length < 11) {
+    return dino
+  }
+  else {
+      const truncatedDino = {
+      species: dino['species'].slice(0,7) + '...',
+      period: dino['period'],
+      carnivore: dino['carnivore'],
+      extinct: dino['extinct'],
+  }
+  return truncatedDino
+  }
+}
+const makeExtinct = function(dino) {
+  const extinctDino = {
+    species: dino['species'],
+    period: dino['period'],
+    carnivore: dino['carnivore'],
+    extinct: true,
+  }
+  return extinctDino
+}
 module.exports = {
   makeDino,
   makeSingular,
   truncateSpecies,
   makeExtinct
 }
+
